0bb9d2855576abab8c2dcd80b7850e22eae2078b
JAGRAT ARORA <jagratarora293@gmail.com>
2025-07-28 19:00:40 +0530

    agents filters

--- src/modules/agents/ui/components/agent-form.tsx
+++ src/modules/agents/ui/components/agent-form.tsx
@@ -29,7 +29,7 @@ export const AgentForm = ({
         trpc.agents.create.mutationOptions({
             onSuccess:() => {
                 queryClient.invalidateQueries(
-                    trpc.agents.getMany.queryOptions(),
+                    trpc.agents.getMany.queryOptions({}),
                 );
                 if(initialValues?.id){
                     queryClient.invalidateQueries(
