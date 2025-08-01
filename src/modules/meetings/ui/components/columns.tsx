e2fee081fadde45ce77584600c3f8b14e983115a
JAGRAT ARORA <jagratarora293@gmail.com>
2025-08-01 23:20:45 +0530

    transcript and chat

--- src/modules/meetings/ui/components/columns.tsx
+++ src/modules/meetings/ui/components/columns.tsx
@@ -1,20 +1,12 @@
 "use client"
 import { format } from "date-fns"
-import humanizeDuration from "humanize-duration"
 import { ColumnDef } from "@tanstack/react-table"
 import { MeetingGetMany } from "../../types"
 import { GeneratedAvatar } from "@/components/generated-avatar"
 import { CircleCheckIcon,CircleXIcon,LoaderIcon,CornerDownRightIcon, ClockArrowUpIcon, ClockFadingIcon } from "lucide-react"
 import { Badge } from "@/components/ui/badge"
-import { cn } from "@/lib/utils"
+import { cn, formatDuration } from "@/lib/utils"
 
-function formatDuration(seconds:number){
-    return humanizeDuration(seconds*1000,{
-        largest:1,
-        round:true,
-        units:["h","m","s"]
-    })
-}
 
 const statusIconMap = {
     upcoming:ClockArrowUpIcon,
